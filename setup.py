b539763193d47356272fd920cda9c32824e039e3
Hoang Khanh Tung <Tung.HK2416641@sis.hust.edu.vn>
2026-03-15 07:45:33 +0700

    Update setup file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="HKT",
+    author_email="ktung1112006@gmail.com",
+    packages=find_packages() # Find all the folder with __init__.py and push them in local package
+)
