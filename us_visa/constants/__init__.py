6a2d4345d32b483551015ffd3d55359a506c4fd9
Hoang Khanh Tung <Tung.HK2416641@sis.hust.edu.vn>
2026-04-30 18:37:11 +0700

    Update constants, config and artifact entity for model training

--- us_visa/constants/__init__.py
+++ us_visa/constants/__init__.py
@@ -58,3 +58,12 @@ Data Transformation ralated constant start with DATA_TRANSFORMATION VAR NAME (in
 DATA_TRANSFORMATION_DIR_NAME: str = "data_transformation"
 DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR: str = "transformed" # transformed data (train.np, test.np)
 DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR: str = "transformed_object" # preprocessing object (preprocessing.pkl) will be stored in this folder
+
+"""
+MODEL TRAINER related constant start with MODEL_TRAINER var name
+"""
+MODEL_TRAINER_DIR_NAME: str = "model_trainer"
+MODEL_TRAINER_TRAINED_MODEL_DIR: str = "trained_model"
+MODEL_TRAINER_TRAINED_MODEL_NAME: str = "model.pkl"
+MODEL_TRAINER_EXPECTED_SCORE: float = 0.6 # Sử dụng cho quá trình model push, nếu model mới có score trên 0.6 thì mới được push lên production, còn ko thì sử dụng model cũ trên server (s3 bucket)
+MODEL_TRAINER_MODEL_CONFIG_FILE_PATH: str = os.path.join("config", "model.yaml")
